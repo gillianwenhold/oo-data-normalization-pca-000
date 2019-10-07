da5901094a0a0ed970b954691a887ef67eeea541
Gillian Wenhold <gwenhold@mac.com>
2019-10-07 11:14:57 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,17 @@
+require "tempfile"
+
 class Song
-  # code goes here
+  attr_accessor :artist, :title
+
+  def initialize
+    @title = ""
+  end
+
+  def slugify
+    contents = self.artist.name + " - " + self.title
+    new_title = self.title.downcase.gsub(" ", "_")
+    file = Tempfile.new([new_title, ".txt"], "tmp")
+    file.write(contents)
+    file.close
+  end
 end
